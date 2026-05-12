f77127c93d64f7198dcdf164f092525c4452da79
samyak-19 <samlinkin19@gmail.com>
2026-05-13 03:41:23 +0530

    project setupt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
+import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Nezus",
   description: "Social Media Platform",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <ClerkProvider>
       <html lang="en">
